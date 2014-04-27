a2a4e08deb11110f60ced736b537c78112d9a7ae
waleedlotfy <waleed.lutfi90@gmail.com>
2014-04-27 09:12:07 +0200

    Netbeans project integration

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: wsl
+ *
+ * Created on April 27, 2014, 9:09 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
